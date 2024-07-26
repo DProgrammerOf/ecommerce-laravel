799d5137c5e6c664889d2f5651e794cab818770e
DProgrammerOf <renanvollenghaupt@icloud.com>
2024-07-26 19:03:47 -0300

    [TESTS] user management - get - create - remove

--- files/tests/Feature/UserManagerTest.php
+++ files/tests/Feature/UserManagerTest.php
@@ -7,6 +7,33 @@ use Tests\TestCase;
 class UserManagerTest extends TestCase
 {
     private string $token = '0981ed2dc48b67053d1b16c748eb9d3a2c0a82fc7e0317851fb78194c36ee2cbd39fe2d935abeb05';
+    private static int $user_id = 0;
+
+    /**
+     * Create user invalid (missing cpf)
+     */
+    public function test_user_create_invalid_params(): void
+    {
+        $response = $this->postJson(
+            'api/users/create',
+            [
+                'username' => 'renan_test',
+                'password' => '123456',
+                'full_name' => 'Renan Test',
+                'email' => 'renanvollenghaupt@icloud.com',
+                'cpf' => '',
+                'date_of_birth' => '1990-01-01',
+                'reference' => null,
+            ],
+            [
+                'Authorization' => 'Bearer '.$this->token
+            ]
+        );
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors'])
+            ->assertJsonCount(1, 'errors');
+    }
 
     /**
      * Create user
@@ -31,6 +58,35 @@ class UserManagerTest extends TestCase
         $response
             ->assertStatus(200)
             ->assertJson(['success' => true]);
+
+        $this->assertNotNull($response['data']['user_id']);
+        $this::$user_id = $response->json('data')['user_id'];
+    }
+
+    /**
+     * Create user invalid (duplicated in db)
+     */
+    public function test_user_create_invalid_duplicated(): void
+    {
+        $response = $this->postJson(
+            'api/users/create',
+            [
+                'username' => 'renan_test',
+                'password' => '123456',
+                'full_name' => 'Renan Test',
+                'email' => 'renanvollenghaupt@icloud.com',
+                'cpf' => '123456789',
+                'date_of_birth' => '1990-01-01',
+                'reference' => null,
+            ],
+            [
+                'Authorization' => 'Bearer '.$this->token
+            ]
+        );
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors'])
+            ->assertJsonCount(3, 'errors');
     }
 
     /**
@@ -54,7 +110,7 @@ class UserManagerTest extends TestCase
      */
     public function test_user_get_one(): void
     {
-        $id = 1;
+        $id = $this::$user_id;
         $response = $this->getJson(
             'api/users/get/'.$id,
             [
@@ -64,7 +120,7 @@ class UserManagerTest extends TestCase
         $response
             ->assertStatus(200)
             ->assertJson(['success' => true])
-            ->assertJsonPath('data.id', $id);
+            ->assertJsonPath('data.user.id', $id);
     }
 
     /**
@@ -81,7 +137,67 @@ class UserManagerTest extends TestCase
         );
         $response
             ->assertStatus(200)
-            ->assertJson(['success' => true, 'data' => null]);
+            ->assertJson(['success' => false, 'message' => 'user not found']);
+    }
+
+    /**
+     * Remove user invalid by id (missing param)
+     */
+    public function test_user_remove_invalid_params(): void
+    {
+        $id = 0;
+        $response = $this->deleteJson(
+            'api/users/remove',
+            [
+
+            ],
+            [
+                'Authorization' => 'Bearer '.$this->token
+            ]
+        );
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors']);
+    }
+
+    /**
+     * Remove user invalid by id (not exist)
+     */
+    public function test_user_remove_invalid_not_exist(): void
+    {
+        $id = 0;
+        $response = $this->deleteJson(
+            'api/users/remove',
+            [
+                'id' => $id
+            ],
+            [
+                'Authorization' => 'Bearer '.$this->token
+            ]
+        );
+        $response
+            ->assertStatus(200)
+            ->assertJson(['success' => false, 'message' => 'user not found']);
+    }
+
+    /**
+     * Remove user valid by id
+     */
+    public function test_user_remove_one(): void
+    {
+        $id = $this::$user_id;
+        $response = $this->deleteJson(
+            'api/users/remove',
+            [
+                'id' => $id
+            ],
+            [
+                'Authorization' => 'Bearer '.$this->token
+            ]
+        );
+        $response
+            ->assertStatus(200)
+            ->assertJson(['success' => true, 'message' => 'user deleted']);
     }
 
 }
